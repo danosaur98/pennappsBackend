347bf00cbce7c032f79d01c7115216e80165498b
danosaur98 <daniel.zhou2012@gmail.com>
2018-01-20 00:22:25 -0500

    heroku

--- app.py
+++ app.py
@@ -1,13 +1,37 @@
-from flask import Flask
+from flask import Flask, jsonify, request
+from flask_pymongo import PyMongo, ObjectId
 
 app = Flask(__name__)
 
-app.config['MONGO_DBNAME'] = 'curatordb'
-app.config['MONGO_URI'] = 'mongodb://admin:password@ds125906.mlab.com:25906/curatordb'
+app.config['MONGO_DBNAME'] = 'pennapps'
+app.config['MONGO_URI'] = 'mongodb://admin:password@ds263837.mlab.com:63837/pennapps'
+
+mongo = PyMongo(app)
+
+
+@app.route("/addLottery", methods=['POST'])
+def add_lottery():
+    lotteries = mongo.db.lotteries
+    lotteries.insert_one({'title': request.args.get("name"), 'total': request.args.get("amount"),
+                          'participants': [request.args.get("ID")]})
+    return jsonify({'result': request.args})
+
+
+@app.route('/getLotteries', methods=['GET'])
+def get_all_articles():
+    lotteries = mongo.db.lotteries
+
+    output = []
+    for q in lotteries.find():
+        output.append({'title': q['title'],
+                       'total': q['total'],
+                       'participants': q['participants']})
+
+    return jsonify({'result': output})
 
 
 @app.route("/")
-def hello():
+def home():
     return "Hello World!"
 
 
